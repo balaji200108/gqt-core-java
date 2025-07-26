691917467a1c2072f385d5ca83c3fd08cafcf0a1
bch62 <bch62@BALAJI.lan>
2025-07-26 07:15:16 +0530

    load all the pattern programs

--- src/com/gqt/corejava/patterns/PatternChar2.java
+++ src/com/gqt/corejava/patterns/PatternChar2.java
@@ -1,20 +1,24 @@
 package com.gqt.corejava.patterns;
+
 import java.util.Scanner;
-public class Pattern20 {
-public static void main(String[] args) {
+
+public class PatternChar2 {
+	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter the size");
 		int n=sc.nextInt();
+		char ch='A';
 		for(int i=1;i<=n;i++) {
-			for(int j=1;j<=n;j++) {
-				System.out.print("-");
-			}
 			
-			for(int j=1;j<=n;j++) {
-				System.out.print("@ ");
+			for(int j=i;j<=n;j++) {
+				System.out.print(" *");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print(ch+" ");
+				ch++;
 			}
 			System.out.println();
 		}
-	}
 
+		}
 }
